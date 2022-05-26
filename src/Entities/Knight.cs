17a5553454a22643236cfb9a42166245e9b7bef9
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-05-26 19:08:50 -0300

    Created Knight class

---
+++ src/Entities/Knight.cs
@@ -0,0 +1,14 @@
+using ProjectGameRPG.src.entities;
+
+namespace ProjectGameRPG.src.Entities
+{
+    public class Knight : Hero
+    {
+        public Knight(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+    }
+}
