8b25b308e94669be8eb01e4a1c3e6f55c544a5f7
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-05-26 22:23:48 -0300

    Created Ninja class

---
+++ src/Entities/Ninja.cs
@@ -0,0 +1,22 @@
+using ProjectGameRPG.src.entities;
+
+namespace ProjectGameRPG.src.Entities
+{
+    public class Ninja : Hero
+    {
+        public Ninja(string Name, int Level) : base(Name, Level)
+        {
+        }
+
+        public string Attack(Hero hero, int bonus){
+            string message;
+            this.ExperiencePoints=+20;
+            message = hero.ReceiveAttack(10 + bonus);
+            bool levelUp = this.IsLevelUp();
+            if(levelUp){
+                return this.Name + " atacou " + hero.getName() + " com bonus de ataque de: " + bonus +  "\n " + hero.getName() + "com bonus de ataque de: " + " subiu de nível!!" + "\n" + message;
+            }
+            return this.Name + " atacou " + hero.getName() + " com bonus de ataque de: " + bonus + "\n" + message;
+        }
+    }
+}
